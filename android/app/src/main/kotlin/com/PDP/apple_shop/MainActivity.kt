2b41e2fa28101f8ad0e0bf52d88a3c7e3af7c6d8
lazizB17 <abdulazizboriyev993@gmail.com>
2022-06-10 12:23:14 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/PDP/apple_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.PDP.apple_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:apple_shop/pages/apple_shop.dart';
+import 'package:apple_shop/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+      routes: {
+        HomePage.id: (context) => const HomePage(),
+        ShopApp.id: (context) => const ShopApp(),
+      },
+    );
+  }
+}
+
